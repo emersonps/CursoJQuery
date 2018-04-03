9d158e85e2ebef0c6feb31747642b0c2390a0ab3
Emerson Pinheiro <emersonline2007@gmail.com>
2018-04-02 22:54:56 -0400

    Realocação de Arquivos para raíz.

---
+++ js/aula-revisao.js
@@ -0,0 +1,29 @@
+$(function(){
+	$('.titulo1').click(function(){
+			var classe = $(this);
+
+			if(classe.hasClass('titulo1')){
+				classe.removeClass('titulo1').fadeOut('fast').fadeIn('fast');
+				classe.addClass('titulo2');
+			}
+			else{
+				classe.removeClass('titulo2').fadeOut('fast').fadeIn('fast');
+				classe.addClass('titulo1');
+			}
+	});
+
+	$('.titulo').click(function(){
+
+		var conteudo = $(this).parent().find('.conteudo'); //parent faz a div voltar ao ancestral dela. (div caixa), sem seguida procurar por um filho caixa que tenha a classa conteúdo.
+		
+		if(!conteudo.hasClass('show')){ //perguntando para o sistema se na div conteudo não tiver a class show, oculta todo o conteúdo da caixa
+			$('.caixa').find('.show').slideUp('fast', function(){
+				$(this).addClass('hide').removeClass('show');
+			});
+
+			conteudo.slideDown('fast',function(){
+				$(this).addClass('show').removeClass('hide');
+			});
+		}
+	});
+});
